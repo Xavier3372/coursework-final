88d606f7b91b55c18c46866e5cae36750ec3f6d3
DudeNav0J <jovan.angkaijie@gmail.com>
2022-02-09 18:01:57 +0800

    autocorrect now better

--- autocorrect.py
+++ autocorrect.py
@@ -3,12 +3,16 @@ from english_words import english_words_set
 from nltk.metrics.distance import edit_distance
 
 
-def autoCorrect(wd):  # Checks edit distance, smaller edit distance = closest word match
-    distance = ((edit_distance(wd, s), s)
-                for s in english_words_set)
-    closest = min(distance)  # is a tuple
-    wordString = ''
-    wordString += closest[1]
-    return wordString
 
+def autoCorrect(wd):  
+    # Find number of edits to convert current word into different possible correct words
+    distance = ((edit_distance(wd, s), s) 
+                for s in english_words_set)
+    # Find word that requires least number of edits to convert from current word to correct word
+    closest = min(distance)
+    wordString = ''
+    # Assign correct word to string
+    wordString += closest[1]
+    # returns string
+    return wordString
 
